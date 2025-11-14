b565a1874c75e788e9cc771441cf2ddffc62ce7d
htbacgiang <80782829+htbacgiang@users.noreply.github.com>
2025-11-14 11:42:23 +0700

    ht

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
-import { Plus_Jakarta_Sans } from 'next/font/google';
 
 // Global polyfill for Promise.withResolvers if not available
 if (typeof Promise !== 'undefined' && !Promise.withResolvers) {
@@ -20,43 +19,44 @@ if (typeof Promise !== 'undefined' && !Promise.withResolvers) {
   };
 }
 
-const jakarta = Plus_Jakarta_Sans({
-  weight: ['400', '500', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-});
-
 let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps: { session, meta, ...pageProps } }) {
   return (
     <>
-      {meta && (
-        <Head>
-          <title>{meta.title}</title>
-          <meta name="description" content={meta.description} />
-          <meta name="keywords" content={meta.keywords} />
-          <meta name="robots" content={meta.robots} />
-          <meta name="author" content={meta.author} />
-          <link rel="canonical" href={meta.canonical} />
-          <meta property="og:title" content={meta.og.title} />
-          <meta property="og:description" content={meta.og.description} />
-          <meta property="og:type" content={meta.og.type} />
-          <meta property="og:image" content={meta.og.image} />
-          <meta property="og:image:width" content={meta.og.imageWidth} />
-          <meta property="og:image:height" content={meta.og.imageHeight} />
-          <meta property="og:url" content={meta.og.url} />
-          <meta name="twitter:card" content={meta.twitter.card} />
-          <meta name="twitter:title" content={meta.twitter.title} />
-          <meta name="twitter:description" content={meta.twitter.description} />
-          <meta name="twitter:image" content={meta.twitter.image} />
-        </Head>
-      )}
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        {meta && (
+          <>
+            <title>{meta.title}</title>
+            <meta name="description" content={meta.description} />
+            <meta name="keywords" content={meta.keywords} />
+            <meta name="robots" content={meta.robots} />
+            <meta name="author" content={meta.author} />
+            <link rel="canonical" href={meta.canonical} />
+            <meta property="og:title" content={meta.og.title} />
+            <meta property="og:description" content={meta.og.description} />
+            <meta property="og:type" content={meta.og.type} />
+            <meta property="og:image" content={meta.og.image} />
+            <meta property="og:image:width" content={meta.og.imageWidth} />
+            <meta property="og:image:height" content={meta.og.imageHeight} />
+            <meta property="og:url" content={meta.og.url} />
+            <meta name="twitter:card" content={meta.twitter.card} />
+            <meta name="twitter:title" content={meta.twitter.title} />
+            <meta name="twitter:description" content={meta.twitter.description} />
+            <meta name="twitter:image" content={meta.twitter.image} />
+          </>
+        )}
+      </Head>
       <SessionProvider session={session}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <div className={jakarta.className}>
+            <div className="font-plus-jakarta">
               <Toaster />
               <Component {...pageProps} />
             </div>
