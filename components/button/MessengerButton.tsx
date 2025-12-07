698405d9a625bd86c55e0eaea2d785dc115c8676
htbacgiang <80782829+htbacgiang@users.noreply.github.com>
2025-12-07 23:57:21 +0700

    h

--- components/button/MessengerButton.tsx
+++ components/button/MessengerButton.tsx
@@ -14,7 +14,19 @@ const MessengerButton: FC = () => {
 
   return (
 
-     <div className={styles.helpContainer}>
+        <div className={styles.helpContainer}>
+
+      <div className={styles.phoneRing}>
+        <div className={styles.phoneRingCircle}></div>
+        <div className={styles.phoneRingCircleFill}>
+        <button
+      onClick={openMessenger}
+    >
+        <Image src={FbMess} alt="face-book icon" width={60} height={60} />
+        </button>
+
+        </div>
+      </div>
       <div className={styles.zaloRing}>
         <div className={styles.zaloRingCircle}></div>
         <div className={styles.zaloRingCircleFill}>
